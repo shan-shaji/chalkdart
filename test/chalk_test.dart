5d635a5ee2df25ea391da4f205cab27fb832b55a
Shan Shaji <developer.shanshaji@gmail.com>
2021-08-04 23:01:51 +0530

    refactor: refactor  code

--- test/chalk_test.dart
+++ test/chalk_test.dart
@@ -18,36 +18,36 @@ void main() {
       expect(RESET, '\033[0m');
     });
 
-    // test('- green color test', () {
-    //   expect(chalk.green('test'), '\x1B[32mtest\x1B[0m');
-    // });
+    test('- green color test', () {
+      expect(chalk.green('test'), '\x1B[32mtest\x1B[0m');
+    });
 
-    // test('- blue color test', () {
-    //   expect(chalk.blue('test'), '\x1B[34mtest\x1B[0m');
-    // });
+    test('- blue color test', () {
+      expect(chalk.blue('test'), '\x1B[34mtest\x1B[0m');
+    });
 
-    // test('- cyan color test', () {
-    //   expect(chalk.cyan('test'), '\x1B[36mtest\x1B[0m');
-    // });
+    test('- cyan color test', () {
+      expect(chalk.cyan('test'), '\x1B[36mtest\x1B[0m');
+    });
 
-    // test('- faint test', () {
-    //   expect(chalk.faint('test'), '\x1B[2mtest\x1B[0m');
-    // });
+    test('- faint test', () {
+      expect(chalk.faint('test'), '\x1B[2mtest\x1B[0m');
+    });
 
-    // test('- italic test', () {
-    //   expect(chalk.italic('test'), '\x1B[3mtest\x1B[0m');
-    // });
+    test('- italic test', () {
+      expect(chalk.italic('test'), '\x1B[3mtest\x1B[0m');
+    });
 
-    // test('- underLined test', () {
-    //   expect(chalk.underLined('test'), '\x1B[4mtest\x1B[0m');
-    // });
+    test('- underLined test', () {
+      expect(chalk.underLined('test'), '\x1B[4mtest\x1B[0m');
+    });
 
-    // test('- strikeThrough test', () {
-    //   expect(chalk.strikeThrough('test'), '\x1B[9mtest\x1B[0m');
-    // });
+    test('- strikeThrough test', () {
+      expect(chalk.strikeThrough('test'), '\x1B[9mtest\x1B[0m');
+    });
 
-    // test('- bold test', () {
-    //   expect(chalk.bold('test'), '\x1B[1mtest\x1B[0m');
-    // });
+    test('- bold test', () {
+      expect(chalk.bold('test'), '\x1B[1mtest\x1B[0m');
+    });
   });
 }
