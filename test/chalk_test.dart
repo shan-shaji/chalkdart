f3f89a9a06616324cf3234108a586b3cecaa4985
Shan Shaji <developer.shanshaji@gmail.com>
2021-08-22 19:03:06 +0530

    fix: fix tests

--- test/chalk_test.dart
+++ test/chalk_test.dart
@@ -17,83 +17,48 @@ void main() {
       var h = chalk.red('test');
       expect(
         h,
-        '\u001b[' +
-            Color.red.on.toString() +
-            'm$test' +
-            '\u001b[${Color.red.off.toString()}m',
+        '\u001b[${Color.red.on.toString()}mtest\u001b[${Color.red.off.toString()}m',
       );
     });
 
     test('- green color test', () {
-      expect(
-          chalk.green('test'),
-          '\u001b[' +
-              Color.green.on.toString() +
-              'm$test' +
-              '\u001b[${Color.green.off.toString()}m');
+      expect(chalk.green('test'),
+          '\u001b[${Color.green.on.toString()}mtest\u001b[${Color.green.off.toString()}m');
     });
 
     test('- blue color test', () {
-      expect(
-          chalk.blue('test'),
-          '\u001b[' +
-              Color.blue.on.toString() +
-              'm$test' +
-              '\u001b[${Color.blue.off.toString()}m');
+      expect(chalk.blue('test'),
+          '\u001b[${Color.blue.on.toString()}mtest\u001b[${Color.blue.off.toString()}m');
     });
 
     test('- cyan color test', () {
-      expect(
-          chalk.cyan('test'),
-          '\u001b[' +
-              Color.cyan.on.toString() +
-              'm$test' +
-              '\u001b[${Color.cyan.off.toString()}m');
+      expect(chalk.cyan('test'),
+          '\u001b[${Color.cyan.on.toString()}mtest\u001b[${Color.cyan.off.toString()}m');
     });
 
     test('- faint test', () {
-      expect(
-          chalk.faint('test'),
-          '\u001b[' +
-              Mod.dim.on.toString() +
-              'm$test' +
-              '\u001b[${Mod.dim.off.toString()}m');
+      expect(chalk.faint('test'),
+          '\u001b[${Mod.dim.on.toString()}mtest\u001b[${Mod.dim.off.toString()}m');
     });
 
     test('- italic test', () {
-      expect(
-          chalk.italic('test'),
-          '\u001b[' +
-              Mod.italic.on.toString() +
-              'm$test' +
-              '\u001b[${Mod.italic.off.toString()}m');
+      expect(chalk.italic('test'),
+          '\u001b[${Mod.italic.on.toString()}mtest\u001b[${Mod.italic.off.toString()}m');
     });
 
     test('- underLined test', () {
-      expect(
-          chalk.underLine('test'),
-          '\u001b[' +
-              Mod.underline.on.toString() +
-              'm$test' +
-              '\u001b[${Mod.underline.off.toString()}m');
+      expect(chalk.underLine('test'),
+          '\u001b[${Mod.underline.on.toString()}mtest\u001b[${Mod.underline.off.toString()}m');
     });
 
     test('- strikeThrough test', () {
-      expect(
-          chalk.strikeThrough('test'),
-          '\u001b[' +
-              Mod.strikeThrough.on.toString() +
-              'm$test' +
-              '\u001b[${Mod.strikeThrough.off.toString()}m');
+      expect(chalk.strikeThrough('test'),
+          '\u001b[${Mod.strikeThrough.on.toString()}mtest\u001b[${Mod.strikeThrough.off.toString()}m');
     });
 
     test('- bold test', () {
-      expect(
-          chalk.bold('test'),
-          '\u001b[' +
-              Mod.bold.on.toString() +
-              'm$test' +
-              '\u001b[${Mod.bold.off.toString()}m');
+      expect(chalk.bold('test'),
+          '\u001b[${Mod.bold.on.toString()}mtest\u001b[${Mod.bold.off.toString()}m');
     });
   });
 }
